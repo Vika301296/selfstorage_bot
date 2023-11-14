214317e64e4ddc448bb5edbc0cf33ba51a13e452
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-11-14 19:36:26 +0300

    Add models

--- selfstorage/bot/admin.py
+++ selfstorage/bot/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Belongings, User
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Belongings)
--- selfstorage/bot/models.py
+++ selfstorage/bot/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='ФИО')
+    telegram_username = models.CharField(
+        max_length=200,
+        verbose_name='Аккаунт в телеграмме', blank=True)
+    phonenumber = models.CharField(
+        'Номер телефона', max_length=20)
+    pd_contract = models.FileField(
+        verbose_name='Договор об использовании ПД'
+    )
+    storage_unit = models.CharField(
+        max_length=20,
+        verbose_name='Склад хранения')
+    box_type = models.CharField(
+        max_length=20,
+        verbose_name='Вид бокса')
+    lease_start_date = models.DateTimeField(
+        verbose_name='Дата начала аренды'
+    )
+    lease_end_date = models.DateTimeField(
+        verbose_name='Дата окончания аренды'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
+class Belongings(models.Model):
+    owner = models.ForeignKey(
+        User,
+        related_name='belongings',
+        verbose_name='Кому принадлежат вещи',
+        on_delete=models.CASCADE
+    )
+    description = models.CharField(
+        max_length=200,
+        verbose_name='Cписок вещей')
+
+    class Meta:
+        verbose_name = 'Вещи'
+    verbose_name_plural = 'Вещи'
