2c7945b7c973885c5b9ad2fde54461273fe45bbe
Roman Bafanov <108800547+RomanBafanov@users.noreply.github.com>
2023-11-15 10:41:21 +0300

    changed order_box.py
    button implemented "Назад"

--- selfstorage/bot/handlers/default_handlers/order_box.py
+++ selfstorage/bot/handlers/default_handlers/order_box.py
@@ -5,4 +5,8 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 @dp.callback_query(F.data == "Заказать бокс")
 async def location_determination(callback: types.CallbackQuery):
-    await callback.message.answer("Заказываем бокс")
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(
+        text="Назад", callback_data="Старт")
+    )
+    await callback.message.answer("Заказываем бокс", reply_markup=builder.as_markup())
