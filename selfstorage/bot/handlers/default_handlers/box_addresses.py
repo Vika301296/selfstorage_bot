e2beea7b46bbe46243958cd91894a5de4d9332e6
Roman Bafanov <108800547+RomanBafanov@users.noreply.github.com>
2023-11-16 09:36:23 +0300

    changed box_addresses.py
    button implemented

--- selfstorage/bot/handlers/default_handlers/box_addresses.py
+++ selfstorage/bot/handlers/default_handlers/box_addresses.py
@@ -5,4 +5,16 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 @dp.callback_query(F.data == "Адреса")
 async def location_determination(callback: types.CallbackQuery):
-    await callback.message.answer("Список адресов")
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(
+        text="Заказать бокс", callback_data="Заказать бокс")
+    )
+    builder.row(types.InlineKeyboardButton(
+        text="Назад", callback_data="Старт")
+    )
+    await callback.message.answer("<b>Список адресов:</b>\n"
+                                  "\n1. <u>Ленина 56</u>\n"
+                                  "2. <u>Суздальский 32</u>\n"
+                                  "3. <u>Хакурате 325</u>\n"
+                                  "4. <u>Митина 12</u>\n"
+                                  "5. <u>Садовая 281</u>", reply_markup=builder.as_markup())
