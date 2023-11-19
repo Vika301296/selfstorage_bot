1a2637dffd97dea02e9864bcb4ceb9f046ab3460
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-11-20 01:23:10 +0300

    flake8 fixes

--- selfstorage/bot/handlers/default_handlers/order_box.py
+++ selfstorage/bot/handlers/default_handlers/order_box.py
@@ -7,7 +7,8 @@ import json
 
 @dp.callback_query(F.data == "Заказать бокс")
 async def location_determination(callback: types.CallbackQuery):
-    # markup = types.ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+    # markup = types.ReplyKeyboardMarkup(
+    #     keyboard=keyboard, resize_keyboard=True)
     # registered = запрос к бд есть ли пользователь в бд !!!
     tg_id = callback.from_user.id
     django_url = "http://127.0.0.1:8000/bot/check_registration/"
@@ -21,7 +22,8 @@ async def location_determination(callback: types.CallbackQuery):
             data_from_db = response.json()
             print(data_from_db)
         except json.decoder.JSONDecodeError:
-            print("Error decoding JSON: Empty response or invalid JSON format.")
+            print(
+                "Error decoding JSON: Empty response or invalid JSON format.")
     else:
         print(f"Error: {response.status_code} - {response.text}")
 
@@ -48,31 +50,32 @@ async def location_determination(callback: types.CallbackQuery):
         builder.row(types.InlineKeyboardButton(
             text="Назад", callback_data="Старт")
         )
-        await callback.message.answer("Выберите адрес склада или мы можем забрать вещи сами!",
-                                      reply_markup=builder.as_markup())
+        await callback.message.answer(
+            "Выберите адрес склада или мы можем забрать вещи сами!",
+            reply_markup=builder.as_markup())
 
 
 @dp.callback_query(F.data == "Адреса боксов")
 async def location_determination(callback: types.CallbackQuery):
     builder = InlineKeyboardBuilder()
-    builder.row(types.InlineKeyboardButton(
-        text="1. Ленина 56", callback_data="Ленина 56")
-    )
-    builder.row(types.InlineKeyboardButton(
-        text="2. Суздальский 32", callback_data="Суздальский 32")
-    )
-    builder.row(types.InlineKeyboardButton(
-        text="3. Хакурате 325", callback_data="Хакурате 325")
-    )
-    builder.row(types.InlineKeyboardButton(
-        text="4. Митина 12", callback_data="Митина 12")
-    )
-    builder.row(types.InlineKeyboardButton(
-        text="5. Садовая 281", callback_data="Садовая 281")
-    )
-    builder.row(types.InlineKeyboardButton(
-        text="Назад", callback_data="Заказать бокс")
-    )
+    django_url = "http://127.0.0.1:8000/bot/storage_address/"
+
+    response = requests.get(django_url)
+
+    addresses = {}
+    if response.status_code == 200:
+        try:
+            addresses = response.json()
+            print(addresses)
+        except json.decoder.JSONDecodeError:
+            print(
+                "Error decoding JSON: Empty response or invalid JSON format.")
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+    for address in addresses:
+        builder.row(types.InlineKeyboardButton(
+            text=f"{address}", callback_data=f"{address}"))
+
     await callback.message.answer("Выберите адрес бокса",
                                   reply_markup=builder.as_markup())
 
@@ -105,7 +108,8 @@ async def location_determination(callback: types.CallbackQuery):
                                   reply_markup=builder.as_markup())
 
 
-@dp.callback_query(F.data.in_(["1,5м.кв", "2м.кв", "3м.кв", "4м.кв", "5м.кв", "10м.кв"]))
+@dp.callback_query(F.data.in_(
+        ["1,5м.кв", "2м.кв", "3м.кв", "4м.кв", "5м.кв", "10м.кв"]))
 async def location_determination(callback: types.CallbackQuery):
     box_size = callback.data
     builder = InlineKeyboardBuilder()
@@ -116,7 +120,9 @@ async def location_determination(callback: types.CallbackQuery):
                                   reply_markup=builder.as_markup())
 
 
-@dp.callback_query(F.data.in_(["Ленина 56", "Суздальский 32", "Хакурате 325", "Митина 12", "Садовая 281"]))
+@dp.callback_query(F.data.in_(
+        ["Ленина 56", "Суздальский 32", "Хакурате 325",
+         "Митина 12", "Садовая 281"]))
 async def location_determination(callback: types.CallbackQuery):
     address_box = callback.data
     builder = InlineKeyboardBuilder()
--- selfstorage/bot/urls.py
+++ selfstorage/bot/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import bot_command, check_registration
-from .views import create_user, my_belongings, storage_address
+from .views import create_user, my_belongings, get_storage_address
 
 urlpatterns = [
     path('bot_command/', bot_command, name='bot_command'),
     path('create_user/', create_user, name='create_user'),
-    path('storage_address/', storage_address, name='storage_address'),
+    path('storage_address/', get_storage_address, name='storage_address'),
     path('my_belongings/', my_belongings, name='my_belongings'),
     path('check_registration/', check_registration, name='check_registration')
 ]
